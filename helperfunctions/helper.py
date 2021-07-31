f3e34e9e807694811ce9021af1ba2169b0431bf6
kitsamho <sam@samho.co.uk>
2021-07-31 08:39:12 +0100

    plot changes

--- helperfunctions/helper.py
+++ helperfunctions/helper.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 
 # this is a plotly figure formatting function that can be used on a range of Plotly figure objects
 def plotly_streamlit_layout(fig, barmode=None, barnorm=None, height=None,width=None):
-    fig.update_layout(
+    fig.update_layout(plot_bgcolor='white',
                       barmode=barmode,
                       barnorm=barnorm,
                       height = height,
